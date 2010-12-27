69dc9a1481c0b7fa7ec2c574326b399740124b89
MIYAMOTO Daisuke <dai.0304@gmail.com>
2010-12-27 14:24:00 +0000

    [ECL-68] win用bat微調整

--- org.jiemamy.eclipse.core.build/build.bat
+++ org.jiemamy.eclipse.core.build/build.bat
@@ -1,4 +1,5 @@
 @echo off
-set SCRIPT_CURRENT_DIR=%%~dpz
-ant -DfromShell=true -lib %SCRIPT_CURRENT_DIR%\..\eclipse-headless-builder\ant4eclipse\org.ant4eclipse_1.0.0.M4.jar -lib %SCRIPT_CURRENT_DIR%\..\eclipse-headless-builder\ant4eclipse\libs\ %*
+cd /d %~dp0
+ant -DfromShell=true -lib ..\eclipse-headless-builder\ant4eclipse\org.ant4eclipse_1.0.0.M4.jar -lib ..\eclipse-headless-builder\ant4eclipse\libs\ %*
+
 
